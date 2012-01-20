d2dec5ced2a7e508a2b2527101e859ce97dfa025
James Dennis <jd@j2labs.net>
2012-01-20 14:01:33 -0500

    Removed NUMA toggle from base script

--- tweaks/ubuntu.sh
+++ tweaks/ubuntu.sh
@@ -3,9 +3,6 @@ echo "fs.file-max = 1000000" >> /etc/sysctl.conf
 echo "ubuntu	soft	nofile	1000000" >> /etc/security/limits.conf
 echo "ubuntu	hard	nofile	1000000" >> /etc/security/limits.conf
 
-### Disable NUMA: http://www.mongodb.org/display/DOCS/NUMA
-echo 0 > /proc/sys/vm/zone_reclaim_mode
-
 ### Update package manager
 apt-get update
 
