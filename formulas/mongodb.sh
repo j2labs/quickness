31b3b716e57f13bbceae388082046b44b1c50c24
James Dennis <jd@j2labs.net>
2012-01-20 21:42:17 -0500

    Adding MongoDB support and update the comments in the Brubeck installer

---
+++ formulas/mongodb.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+### This script follows the instructions found on MongoDB's website.
+### www.mongodb.org/display/DOCS/Ubuntu+and+Debian+packages
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+sudo echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" >> /etc/apt/source.list
+sudo apt-get update
+sudo apt-get install mongodb-10gen
