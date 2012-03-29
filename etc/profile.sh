b61788276552843d3eac5629a81dac66f7d42a08
James Dennis <jd@j2labs.net>
2012-03-28 22:24:07 -0400

    Turned the profile into a proper profile. :)

--- etc/profile.sh
+++ etc/profile.sh
@@ -4,9 +4,40 @@
 
 THIS_SCRIPT=${BASH_SOURCE[@]}
 
+### Colorful prompt with special colors for root
+if [ $USER = "root" ]; then
+	PS_COLOR="31m"
+else
+	PS_COLOR="32m"
+        if [ ! -d "$HOME/.tmp" ]; then
+            mkdir $HOME/.tmp
+        fi
+	export TMPDIR="$HOME/.tmp"
+fi
+export PS1="\n[01;$PS_COLOR\u@\h[0m : \t : [01;34m\w[0m\n\$ "
+
+### default arguments to commands
+alias ls="ls -F -G"
+alias rm="rm -i"
+alias cp="cp -i"
+alias mv="mv -i"
+
+### helpers
+alias l="ls"
+alias sl="ls"
+alias lss="ls --hide=*~"
+alias la="ls -a"
+alias ll="ls -l"
+alias lal="ls -al"
+alias lsd="ls -l | grep ^d"
+alias psg="ps ax | grep"
+alias clpy="rm -f *~ *pyc"
+alias pwdd="pwd -P"
+alias sudoo="sudo /bin/bash -l"
+
 
 ###
-### Settings
+### Quickness Settings
 ###
 
 export QUICKNESS_REPO=$(cd $(dirname $(dirname $THIS_SCRIPT)); pwd)
@@ -43,3 +74,10 @@ quick_install() {
     fi
 }
 
+quick_tweaks() {
+    ls $QK_TWEAKS
+}
+
+quick_formulas() {
+    ls $QK_FORMULAS
+}
